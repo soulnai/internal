bb2a32a04af14f7e37f9ccabc59e9d7a9478c75d
Alexander Vasilyev <soulnai@gmail.com>
2014-11-14 13:47:50 +0200

    test 22

--- tests/functional/test_lohika.py
+++ tests/functional/test_lohika.py
@@ -19,7 +19,7 @@ class LohikaReferralBonusCheck(unittest.TestCase):
 
     def setUp(self):
         """Open referral bonus page"""
-        self.driver = webdriver.Remote("http://127.0.0.2:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNITWITHJS)
+        self.driver = webdriver.Remote("http://127.0.0.2:4444/wd/hub", webdriver.DesiredCapabilities.FIREFOX)
         """username = os.environ["SAUCE_USERNAME"]
         access_key = os.environ["SAUCE_ACCESS_KEY"]
         desired_capabilities=['DesiredCapabilities.HTMLUNITWITHJS']
@@ -27,7 +27,6 @@ class LohikaReferralBonusCheck(unittest.TestCase):
         desired_capabilities["tunnel-identifier"] = os.environ["HTMLUNIT"]
         hub_url = "%s:%s@localhost:4445" % (username, access_key)
         driver = webdriver.Remote(desired_capabilities=desired_capabilities, command_executor="http://%s/wd/hub" % hub_url)"""
-        self.driver.setJavascriptEnabled(true);
         self.wait = WebDriverWait(self.driver, 10)
         self.driver.get("http://www.lohika.com.ua/#vacancies-referral")
 
