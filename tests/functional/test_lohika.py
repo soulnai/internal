53470cc6d82c601abf4bc261735701cc5c3df3f8
Alexander Vasilyev <soulnai@gmail.com>
2014-11-17 11:32:09 +0200

    check

--- tests/functional/test_lohika.py
+++ tests/functional/test_lohika.py
@@ -14,7 +14,6 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-
 class LohikaReferralBonusCheck(unittest.TestCase):
 
     def setUp(self):
