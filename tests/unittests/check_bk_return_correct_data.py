9e1ab1f1344f5e7d8682e9a26730cbfd92cdd6e3
Alexander Vasilyev <soulnai@gmail.com>
2014-11-20 16:57:41 +0200

    parameters file added to travis

--- tests/unittests/check_bk_return_correct_data.py
+++ tests/unittests/check_bk_return_correct_data.py
@@ -16,14 +16,18 @@ file.close()
 
 @pytest.mark.parametrize('param1', file_lines)
 @pytest.mark.parametrize('param2', file_lines)
-def test_bk_output_length(param1, param2):
-    game = bulls.utility_functions.GameUtilityFunctions()
-    func_response = game.return_bulls_cows_to_file(param1, param2)
-    assert len(func_response) == 2
+class TestClass:
 
-@pytest.mark.parametrize('param1', file_lines)
-@pytest.mark.parametrize('param2', file_lines)
-def test_bk_output_is_digit(param1, param2):
-    game = bulls.utility_functions.GameUtilityFunctions()
-    func_response = game.return_bulls_cows_to_file(param1, param2)
-    assert func_response.isdigit()
+
+    def test_bk_output_length(self, param1, param2):
+        game = bulls.utility_functions.GameUtilityFunctions()
+        func_response = game.return_bulls_cows_to_file(param1, param2)
+        assert len(func_response) == 2
+
+    def test_bk_output_is_digit(self, param1, param2):
+        game = bulls.utility_functions.GameUtilityFunctions()
+        func_response = game.return_bulls_cows_to_file(param1, param2)
+        assert func_response.isdigit()
+
+if __name__ == '__main__':
+    pytest.main('check_bk_return_correct_data.py -vv')
